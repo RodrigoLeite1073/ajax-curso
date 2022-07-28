1f611577decf937a96b6fad13c7c7cea3b71d59f
Rodrigo Leite <rodrigoleite1073@gmail.com>
2022-07-28 02:11:52 -0500

    fin primera clase

--- js/ajax.js
+++ js/ajax.js
@@ -1,1 +1,37 @@
-const d = document;
+(() => {
+  const xhr = new XMLHttpRequest(),
+    $xhr = document.getElementById("xhr"),
+    $fragment = document.createDocumentFragment();
+
+  xhr.addEventListener("readystatechange", (e) => {
+    if (xhr.readyState !== 4) {
+      $xhr.innerHTML = `<h2>CARGANDO...</h2>`;
+      return;
+    }
+    if (xhr.status >= 200 && xhr.status < 300) {
+      //console.log(xhr.responseText);
+      let json = JSON.parse(xhr.responseText);
+
+      json.forEach((el) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `<h2>${el.name}</h2>
+        <h4>${el.phone}</h4>
+        <h4>${el.email}</h4>`;
+        $fragment.appendChild($li);
+      });
+      setTimeout(() => {
+        $xhr.innerHTML = "";
+        $xhr.appendChild($fragment);
+      }, 3000);
+    } else {
+      console.log("error");
+      const meessage = xhr.statusText || "Ocurrio un error";
+      $xhr.innerHTML = `<h2>${xhr.status}</h2>
+      <h3>${meessage}</h3>`;
+    }
+  });
+
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/users");
+
+  xhr.send();
+})();
