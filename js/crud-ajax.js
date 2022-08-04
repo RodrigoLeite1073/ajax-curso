ce26cfcf40b3a6e9d188537d7236f5260f4631ca
Rodrigo Leite <rodrigoleite1073@gmail.com>
2022-08-03 23:32:17 -0500

    crud con ajax

--- js/crud-ajax.js
+++ js/crud-ajax.js
@@ -1,2 +1,15 @@
-console.log("hola js");
-alert("hola js");
+const d = document,
+  $table = d.querySelector(".crud-table"),
+  $form = d.querySelector(".crud-form"),
+  $title = d.querySelector(".crud-title"),
+  $template = d.getElementById("crud-template").content,
+  $fragment = d.createDocumentFragment();
+
+const ajax = (options) => {
+  let [url, method, success, error, data] = options;
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", () => {});
+
+  xhr.open(method || "GET", url);
+};
