71174d6c96c86c5e9ada20943f9359475d61ad5e
Rodrigo Leite <rodrigoleite1073@gmail.com>
2022-08-07 22:32:55 -0500

    añadiendo opiciones de crud

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
 import { actionsBtn, getAll, submitBtn } from "./crudA.js";
 
-const d = document;
+const d = document,
+  $select = d.getElementById("select"),
+  miStorage = window.localStorage;
+
+$select.addEventListener("change", (e) => {
+  miStorage.setItem("option", e.target.value);
+});
 
 d.addEventListener("DOMContentLoaded", () => {
   getAll();
   submitBtn();
   actionsBtn();
+
+  let option = miStorage.getItem("option");
+  if (option) {
+    alert(option);
+    $select.value = option;
+  }
 });
